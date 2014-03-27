0bfdb65e4538507310373935efc01c0ba0ec3aaf
sepmein <sepmein@gmail.com>
2014-03-27 16:41:19 +0800

    npm test

--- test/index.js
+++ test/index.js
@@ -1,27 +1,38 @@
 /**
  * Created by Spencer on 14-3-19.
  */
-var fs = require('fs');
+//var fs = require('fs');
+//
+//var readStream = fs.createReadStream('D:\\Program Files\\Battle.net\\Hearthstone\\base-Win.mpq');
+//var writeStream = fs.createWriteStream('F:\\sepmein\\receive\\temp');
+//writeStream.on('pipe', function () {
+//    console.log('pipe started');
+//});
+//
+////readStream.pipe(writeStream);
+//
+//var net = require('net');
+//var server = net.createServer(
+//    function (connection) {
+//        connection.on('data', function (chunck) {
+//            console.dir(chunck);
+//        });
+//    });
+//
+//server.listen(8088);
+//
+//var client = net.connect({port: 8088},
+//    function () {
+//        readStream.pipe(client);
+//    });
 
-var readStream = fs.createReadStream('D:\\Program Files\\Battle.net\\Hearthstone\\base-Win.mpq');
-var writeStream = fs.createWriteStream('F:\\sepmein\\receive\\temp');
-writeStream.on('pipe', function () {
-    console.log('pipe started');
+var should = require('should');
+
+var user = {
+    name: 'spencer'
+}
+describe('First test', function () {
+    it('user\'s name should be spencer', function () {
+        user.should.have.property('name', 'spencer');
+    });
 });
-
-//readStream.pipe(writeStream);
-
-var net = require('net');
-var server = net.createServer(
-    function (connection) {
-        connection.on('data', function (chunck) {
-            console.dir(chunck);
-        });
-    });
-
-server.listen(8088);
-
-var client = net.connect({port: 8088},
-    function () {
-        readStream.pipe(client);
-    });
